b562b8799c9234d287a232030062493f2225f5bb
David Alejandro <davidlondono9@gmail.com>
2016-10-24 16:44:59 -0500

    feat(votePopular): fixed to use pre execs on it

--- src/plugins/PopularStream.js
+++ src/plugins/PopularStream.js
@@ -1,6 +1,10 @@
 /**
  * Created by david on 10/4/16.
  */
+import _debug from 'debug';
+
+const debugInfo = _debug('readLeaf:hotStream:info');
+const debugTrimInfo = _debug('readLeaf:trim:info');
 /**
  *
  * @param {Object} list
@@ -12,19 +16,35 @@ export default (list, {
   epoch = new Date(2015, 10, 1).getTime() / 100000,
 } = {}) => {
   list.votePopular = (member) => {
-    this.incrementBy({
-      member,
-      by: 2 * ((new Date().getTime() - epoch) / halfLife),
-    });
-    this.trim(10000);
+    debugInfo(`will vote popular on list ${this.name}`, member);
+
+    // exec pre votes
+    return this.execPre('votePopular', member)
+
+      // increment the vote on one with formula: add 2**((now - epoch) / half_life)
+      .then(() => this.incrementBy({
+        member,
+        by: 2 * ((new Date().getTime() - epoch) / halfLife),
+      }))
+      .then((add) => {
+        debugInfo(`added vote popular on list ${this.name}`, add);
+        this.trim(10000);
+      });
   };
 
-  list.trim = async(amount) => {
-    if (Math.random() < (2 / amount)) {
-      await list.removeRangeByRank({
-        min: 0,
-        max: -amount,
-      });
-    }
-  };
+  if (!list.trim) {
+    list.trim = function trim(amount) {
+      this.execPre('trim', amount)
+        .then(() => {
+          if (Math.random() < (2 / amount)) {
+            debugTrimInfo(`trimming ${this.name} with amount`, amount);
+            return this.removeRangeByRank({
+              min: 0,
+              max: -amount,
+            });
+          }
+          return null;
+        });
+    };
+  }
 };
